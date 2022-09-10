aadc07e7296846ade1c2ab7cba166b701d569652
lucas <109874477+silva4dev@users.noreply.github.com>
2022-09-10 17:07:40 -0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,8 +1,5 @@
-import { join } from "https://deno.land/std/path/mod.ts";
-import { parse } from "https://deno.land/std/encoding/csv.ts";
-import { BufReader } from "https://deno.land/std/io/bufio.ts";
-
-import { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+import { join } from "https://deno.land/std@0.155.0/path/mod.ts";
+import { parse } from "https://deno.land/std@0.155.0/encoding/csv.ts";
 
 interface Planet {
   [key: string]: string;
@@ -10,27 +7,26 @@ interface Planet {
 
 async function loadPlanetDataPeriods() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
-
   const file = await Deno.open(path);
-  const bufReader = new BufReader(file);
-
-  const result = await parse(bufReader, {
+  const result = await parse(await Deno.readTextFile(path), {
     skipFirstRow: true,
     comment: "#",
   });
 
-  // Close file resource id (rid) to avoid leaking resources.
   Deno.close(file.rid);
-
   const planets = (result as Array<Planet>).filter((planet) => {
     const planetaryRadius = Number(planet["koi_prad"]);
     const stellarRadius = Number(planet["koi_srad"]);
     const stellarMass = Number(planet["koi_smass"]);
-
-    return planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
-      stellarRadius > 0.99 && stellarRadius < 1.01 &&
-      stellarMass > 0.78 && stellarMass < 1.04;
+    return (
+      planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 &&
+      planetaryRadius < 1.5 &&
+      stellarRadius > 0.99 &&
+      stellarRadius < 1.01 &&
+      stellarMass > 0.78 &&
+      stellarMass < 1.04
+    );
   });
 
   return planets.map((planet) => Number(planet["koi_period"]));
